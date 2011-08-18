7f8cfbfbc08ec7c5b41a6077f2415ab9fec3110f
Kalman Hosszu <hosszu.kalman@gmail.com>
2011-08-18 11:48:32 +0200

    Implementing invoke, to execute button.

--- wysiwyg_plugins/quote/quote.js
+++ wysiwyg_plugins/quote/quote.js
@@ -10,5 +10,12 @@
 
 Drupal.wysiwyg.plugins['quote'] = {
   
+  /**
+   * Execute the button.
+   */
+  invoke: function(data, settings, instanceId) {
+    Drupal.wysiwyg.instances[instanceId].insert('[quote]' + data.content + '[/quote]');
+  }
+}
   
 })(jQuery);
