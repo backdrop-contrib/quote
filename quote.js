a4cb5f591eb16792b854ced7637f4abd81a28e77
Zen <karthik@21209.no-reply.drupal.org>
2012-01-18 13:47:38 +0530

    Multiple nested quotes fail without splitting the selector.

--- quote.js
+++ quote.js
@@ -4,7 +4,8 @@ Drupal.behaviors.quote = {
   attach: function() {
     var level = Drupal.settings.quote_nest - 1;
     if (level >= 0) {
-      $('blockquote.quote-nest-1 blockquote.quote-msg:eq(' + level + ')')
+      var top = $('blockquote.quote-nest-1');
+      $('blockquote.quote-msg:eq(' + level + ')', top)
       .hide()
       .after('<div class="quote-snip">' + Drupal.t('<a href="#">[snip]</a>') + '</div>')
       .next('.quote-snip')
