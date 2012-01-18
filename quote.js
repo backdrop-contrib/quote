52f23f783d8c30bcc99a2199dc0ee680dfd1c47e
Zen <karthik@21209.no-reply.drupal.org>
2012-01-18 13:35:16 +0530

    Remove unused variable.

--- quote.js
+++ quote.js
@@ -4,7 +4,7 @@ Drupal.behaviors.quote = {
   attach: function() {
     var level = Drupal.settings.quote_nest - 1;
     if (level >= 0) {
-      var top = $('blockquote.quote-nest-1 blockquote.quote-msg:eq(' + level + ')')
+      $('blockquote.quote-nest-1 blockquote.quote-msg:eq(' + level + ')')
       .hide()
       .after('<div class="quote-snip">' + Drupal.t('<a href="#">[snip]</a>') + '</div>')
       .next('.quote-snip')
