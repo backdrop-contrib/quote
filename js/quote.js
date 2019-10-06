765f4a92b46573f05ced36d2fde79363213263f1
Andrei Ivnitskii <ivnish.dev@gmail.com>
2019-10-06 18:50:51 +0200

    Work in progress

--- js/quote.js
+++ js/quote.js
@@ -16,11 +16,10 @@ Drupal.behaviors.quote = {
       return '';
     }
 
-    var quoteSel = $('.quote-sel a');
+    var commentQuoteSel = $('.comment-quote-sel a');
+    commentQuoteSel.attr('href', '#');
 
-    quoteSel.attr('href', '#');
-
-    quoteSel.click(function(e) {
+    commentQuoteSel.click(function(e) {
       e.preventDefault();
       var selected = getSelectedText();
 
@@ -33,6 +32,52 @@ Drupal.behaviors.quote = {
         commentArea.focus();
       }
 
+    });
+
+    var commentQuoteAll = $('.comment-quote-all a');
+    commentQuoteAll.attr('href', '#');
+
+    commentQuoteAll.click(function(e) {
+      e.preventDefault();
+      var commentArea = $('#edit-comment-body textarea');
+      var parent = $(this).closest('.comment');
+      var username = parent.find('a.username').text();
+      var alltext = parent.find('.field-name-comment-body').text();
+      var curValue = commentArea.val();
+      commentArea.val(curValue + '<blockquote><strong>' + username + ' wrote:</strong> ' + alltext + '</blockquote>');
+      commentArea.focus();
+    });
+
+    var nodeQuoteSel = $('.node-quote-sel a');
+    nodeQuoteSel.attr('href', '#');
+
+    nodeQuoteSel.click(function(e) {
+      e.preventDefault();
+      var selected = getSelectedText();
+
+      if (selected.length) {
+        var commentArea = $('#edit-comment-body textarea');
+        var parent = $(this).closest('.node');
+        var username = parent.find('a.username').first().text();
+        var curValue = commentArea.val();
+        commentArea.val(curValue + '<blockquote><strong>' + username + ' wrote:</strong> ' + selected + '</blockquote>');
+        commentArea.focus();
+      }
+
+    });
+
+    var nodeQuoteAll = $('.node-quote-all a');
+    nodeQuoteAll.attr('href', '#');
+
+    nodeQuoteAll.click(function(e) {
+      e.preventDefault();
+      var commentArea = $('#edit-comment-body textarea');
+      var parent = $(this).closest('.node');
+      var username = parent.find('a.username').first().text();
+      var alltext = parent.find('.field-name-body').text();
+      var curValue = commentArea.val();
+      commentArea.val(curValue + '<blockquote><strong>' + username + ' wrote:</strong> ' + alltext + '</blockquote>');
+      commentArea.focus();
     });
 
   }
