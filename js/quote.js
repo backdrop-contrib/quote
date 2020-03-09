7d834e77690ce001a71dc6763c521b5ccb6e7126
Andrei Ivnitskii <ivnish.dev@gmail.com>
2020-03-09 14:42:38 +0100

    Issue #1900172: Support content types that do not use the default body field

--- js/quote.js
+++ js/quote.js
@@ -16,66 +16,59 @@ Drupal.behaviors.quote = {
       return '';
     }
 
+    // TODO: refactor this
     var commentQuoteSel = $('.comment-quote-sel a');
     commentQuoteSel.attr('href', '#');
+    var commentQuoteAll = $('.comment-quote-all a');
+    commentQuoteAll.attr('href', '#');
+    var nodeQuoteSel = $('.node-quote-sel a');
+    nodeQuoteSel.attr('href', '#');
+    var nodeQuoteAll = $('.node-quote-all a');
+    nodeQuoteAll.attr('href', '#');
+
+    var commentArea = $(Drupal.settings.quote.quote_selector);
+    var curValue = commentArea.val();
 
     commentQuoteSel.click(function(e) {
       e.preventDefault();
       var selected = getSelectedText();
 
       if (selected.length) {
-        var commentArea = $('#edit-comment-body textarea');
         var parent = $(this).closest('.comment');
         var username = parent.find('a.username').text();
-        var curValue = commentArea.val();
         commentArea.val(curValue + '<blockquote><strong>' + username + ' wrote:</strong> ' + selected + '</blockquote>');
         commentArea.focus();
       }
 
     });
 
-    var commentQuoteAll = $('.comment-quote-all a');
-    commentQuoteAll.attr('href', '#');
-
     commentQuoteAll.click(function(e) {
       e.preventDefault();
-      var commentArea = $('#edit-comment-body textarea');
       var parent = $(this).closest('.comment');
       var username = parent.find('a.username').text();
       var alltext = parent.find('.field-name-comment-body').text();
-      var curValue = commentArea.val();
       commentArea.val(curValue + '<blockquote><strong>' + username + ' wrote:</strong> ' + alltext + '</blockquote>');
       commentArea.focus();
     });
-
-    var nodeQuoteSel = $('.node-quote-sel a');
-    nodeQuoteSel.attr('href', '#');
 
     nodeQuoteSel.click(function(e) {
       e.preventDefault();
       var selected = getSelectedText();
 
       if (selected.length) {
-        var commentArea = $('#edit-comment-body textarea');
         var parent = $(this).closest('.node');
         var username = parent.find('a.username').first().text();
-        var curValue = commentArea.val();
         commentArea.val(curValue + '<blockquote><strong>' + username + ' wrote:</strong> ' + selected + '</blockquote>');
         commentArea.focus();
       }
 
     });
 
-    var nodeQuoteAll = $('.node-quote-all a');
-    nodeQuoteAll.attr('href', '#');
-
     nodeQuoteAll.click(function(e) {
       e.preventDefault();
-      var commentArea = $('#edit-comment-body textarea');
       var parent = $(this).closest('.node');
       var username = parent.find('a.username').first().text();
       var alltext = parent.find('.field-name-body').text();
-      var curValue = commentArea.val();
       commentArea.val(curValue + '<blockquote><strong>' + username + ' wrote:</strong> ' + alltext + '</blockquote>');
       commentArea.focus();
     });
