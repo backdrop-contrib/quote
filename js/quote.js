e110d89644d27f36662b6565b7af7159b4670147
Andrei Ivnitskii <ivnish.dev@gmail.com>
2020-03-15 16:30:37 +0100

    Issue #3119907: Fix coding standards

--- js/quote.js
+++ js/quote.js
@@ -3,68 +3,68 @@
   'use strict';
 
   Drupal.behaviors.quote = {
-  attach: function(context, settings) {
+    attach: function (context, settings) {
 
-    var quoteLimit = Drupal.settings.quote.quote_limit;
+      var quoteLimit = Drupal.settings.quote.quote_limit;
 
-    function getSelectedText() {
-      if (document.getSelection) {
-        return document.getSelection().toString().substring(0, quoteLimit);
+      function getSelectedText() {
+        if (document.getSelection) {
+          return document.getSelection().toString().substring(0, quoteLimit);
+        }
+
+        return '';
       }
 
-      return '';
-    }
+      $('.comment-quote-sel a').click(function (e) {
+        e.preventDefault();
+        var selected = getSelectedText();
 
-    $('.comment-quote-sel a').click(function(e) {
-      e.preventDefault();
-      var selected = getSelectedText();
+        if (selected.length) {
+          var commentArea = $(Drupal.settings.quote.quote_selector);
+          var curValue = commentArea.val();
+          var parent = $(this).closest('.comment');
+          var username = parent.find('a.username').text();
+          commentArea.val(curValue + '<blockquote><strong>' + username + ' wrote:</strong> ' + selected + '</blockquote>');
+          commentArea.focus();
+        }
+      });
 
-      if (selected.length) {
+      $('.comment-quote-all a').click(function (e) {
+        e.preventDefault();
         var commentArea = $(Drupal.settings.quote.quote_selector);
         var curValue = commentArea.val();
         var parent = $(this).closest('.comment');
         var username = parent.find('a.username').text();
-        commentArea.val(curValue + '<blockquote><strong>' + username + ' wrote:</strong> ' + selected + '</blockquote>');
+        var alltext = parent.find(Drupal.settings.quote.quote_selector_comment_quote_all).text().substring(0, quoteLimit);
+        commentArea.val(curValue + '<blockquote><strong>' + username + ' wrote:</strong> ' + alltext + '</blockquote>');
         commentArea.focus();
-      }
-    });
+      });
 
-    $('.comment-quote-all a').click(function(e) {
-      e.preventDefault();
-      var commentArea = $(Drupal.settings.quote.quote_selector);
-      var curValue = commentArea.val();
-      var parent = $(this).closest('.comment');
-      var username = parent.find('a.username').text();
-      var alltext = parent.find(Drupal.settings.quote.quote_selector_comment_quote_all).text().substring(0, quoteLimit);
-      commentArea.val(curValue + '<blockquote><strong>' + username + ' wrote:</strong> ' + alltext + '</blockquote>');
-      commentArea.focus();
-    });
+      $('.node-quote-sel a').click(function (e) {
+        e.preventDefault();
+        var selected = getSelectedText();
 
-    $('.node-quote-sel a').click(function(e) {
-      e.preventDefault();
-      var selected = getSelectedText();
+        if (selected.length) {
+          var commentArea = $(Drupal.settings.quote.quote_selector);
+          var curValue = commentArea.val();
+          var parent = $(this).closest('.node');
+          var username = parent.find('a.username').first().text();
+          commentArea.val(curValue + '<blockquote><strong>' + username + ' wrote:</strong> ' + selected + '</blockquote>');
+          commentArea.focus();
+        }
+      });
 
-      if (selected.length) {
+      $('.node-quote-all a').click(function (e) {
+        e.preventDefault();
         var commentArea = $(Drupal.settings.quote.quote_selector);
         var curValue = commentArea.val();
         var parent = $(this).closest('.node');
         var username = parent.find('a.username').first().text();
-        commentArea.val(curValue + '<blockquote><strong>' + username + ' wrote:</strong> ' + selected + '</blockquote>');
+        var alltext = parent.find(Drupal.settings.quote.quote_selector_node_quote_all).text().substring(0, quoteLimit);
+        commentArea.val(curValue + '<blockquote><strong>' + username + ' wrote:</strong> ' + alltext + '</blockquote>');
         commentArea.focus();
-      }
-    });
-
-    $('.node-quote-all a').click(function(e) {
-      e.preventDefault();
-      var commentArea = $(Drupal.settings.quote.quote_selector);
-      var curValue = commentArea.val();
-      var parent = $(this).closest('.node');
-      var username = parent.find('a.username').first().text();
-      var alltext = parent.find(Drupal.settings.quote.quote_selector_node_quote_all).text().substring(0, quoteLimit);
-      commentArea.val(curValue + '<blockquote><strong>' + username + ' wrote:</strong> ' + alltext + '</blockquote>');
-      commentArea.focus();
-    });
-  }
-};
+      });
+    }
+  };
 
 }(jQuery));
